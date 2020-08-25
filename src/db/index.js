d12c62322c71f16e82abfebaf583f7633ef8284b
zhengzhibo <zhibo90@gmaiol.com>
2020-08-26 00:50:10 +0800

    nedb 修改为indexeddb模式

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,7 @@
-import path from 'path';
-import { ipcRenderer } from 'electron';
-
 const Datastore = require('nedb-promises');
 
-const { userData } = ipcRenderer.sendSync('GetSystemInfo');
-const tasks = Datastore.create(path.join(userData, 'db/tasks.db'));
-const setting = Datastore.create(path.join(userData, 'db/setting.db'));
+const tasks = Datastore.create('task');
+const setting = Datastore.create('setting');
 
 export { tasks, setting };
 
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,6 @@ module.exports = {
   runtimeCompiler: true,
   pluginOptions: {
     electronBuilder: {
-      nodeIntegration: true,
-      externals: ['nedb-promises'],
     },
   },
 };
