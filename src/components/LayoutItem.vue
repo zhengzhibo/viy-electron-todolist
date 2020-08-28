3fadad40a7c2c1ec1976a658d47f205f936a9876
zhengzhibo <zhibo90@gmail.com>
2020-08-29 01:29:51 +0800

    layoutitem 组件

---
+++ src/components/LayoutItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="item-container">
+    <span class="item-title">{{title}}</span>
+    <div class="item-content">
+      <slot>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LayoutItem',
+  props: {
+    title: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+.item-container + .item-container {
+  margin-top: 10px;
+}
+
+.item-title {
+  color: var(--font-color);
+  line-height: 2em;
+  filter: brightness(80%);
+}
+</style>
